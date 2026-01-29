4a91fcc442a2e9b48d8d5081dcee312bfea0cdda
gabrieldiem <gabriel.diem5@gmail.com>
2026-01-29 13:26:06 -0300

    feat: added default layers for national limits

---
+++ src/app/config/default-active-layers.config.ts
@@ -0,0 +1,8 @@
+/**
+ * Lista de IDs de capas que deben estar activas por defecto
+ * si el usuario no tiene una configuración guardada
+ */
+export const DEFAULT_ACTIVE_LAYERS: string[] = [
+  'ign-limite-internacional',
+  'ign-limite-provincial',
+];
