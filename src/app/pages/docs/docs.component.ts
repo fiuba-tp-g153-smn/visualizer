72bfc171e66f0dd4304962d3ac1d652fc4023efb
gabrieldiem <gabriel.diem5@gmail.com>
2026-01-31 18:15:46 -0300

    feat: added theme toggle for docs

--- src/app/pages/docs/docs.component.ts
+++ src/app/pages/docs/docs.component.ts
@@ -5,11 +5,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-docs',
   standalone: true,
-  imports: [CommonModule, MarkdownModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [
+    CommonModule,
+    MarkdownModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    MatTooltipModule,
+  ],
   templateUrl: './docs.component.html',
   styleUrl: './docs.component.scss',
 })
@@ -21,11 +29,16 @@ export class DocsComponent {
   ];
 
   activeTopic = 'intro';
+  isDarkMode = false;
 
   constructor(private http: HttpClient) {}
 
   selectTopic(topicId: string) {
     this.activeTopic = topicId;
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
   }
 
   get currentDocUrl() {
