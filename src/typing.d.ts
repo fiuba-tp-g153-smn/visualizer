0e390f9062d39fc10dfc1dda0e90e3283d5d45d1
gabrieldiem <gabriel.diem5@gmail.com>
2026-03-09 23:15:45 -0300

    config: removed old use_mock_tiles venv

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -3,7 +3,6 @@ declare var $ENV: Env;
 interface Env {
   DATA_SERVICE_BASE_URL: string;
   ALERTS_SERVICE_BASE_URL: string;
-  USE_MOCK_TILES: boolean;
   TILE_FORMAT: string;
   APP_HOST_PORT: integer;
   DOCS_URL: string;
