5e461569b9bab4172d31371fe18d41359b4aabc5
Máximo Gismondi <maximogismondi@gmail.com>
2026-05-16 18:52:24 -0300

    chore: wire SMN token prompt env through build and docs

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         ALERTS_SERVICE_BASE_URL: JSON.stringify(
           process.env.ALERTS_SERVICE_BASE_URL || 'http://localhost:8080',
         ),
+        SMN_API_PROMPT_FOR_TOKEN: JSON.stringify(process.env.SMN_API_PROMPT_FOR_TOKEN || 'true'),
         TILE_FORMAT: JSON.stringify(process.env.TILE_FORMAT || 'webp'),
         APP_HOST_PORT: JSON.stringify(process.env.APP_HOST_PORT || '4200'),
         DOCS_URL: JSON.stringify(process.env.DOCS_URL || 'https://docs.mapasmn.com/visualizer/'),
